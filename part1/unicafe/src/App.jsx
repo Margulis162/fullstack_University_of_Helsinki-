c83f6ae574bbde3287cff33c99eaa80a8ac4dee6
Margulis162 <markmargulis@proton.me>
2023-12-26 20:18:46 -0500

    last commit for today, got stuck at the issue with async at line 31 handleClickPpsitive App.jsx Suggested work around in the article does not seem to work

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,24 +1,49 @@
 import { useState } from 'react'
 import './App.css'
 
-const Header = (props) =>{
+const Header = (p) =>{
   return(
     <div className='header'>
-      <h1>{props.message} </h1>
+      <h1>{p.message} </h1>
     </div>
+  )
+}
+
+const Button = (p) =>{
+  return(
+    <button onClick={p.clickHandler}>
+      {p.text}
+    </button>
   )
 }
 
 
 const App = () => {
   // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
+  const [good, setGood] = useState(0) //changing initial useState to 1 does the job but I feel like it's illigal 
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+
+  const handleClick = () =>{
+    console.log('click click')
+  }
+
+  const handleClickPositive = () =>{
+
+    // suggested workaround doesn't work as expected, did I do it right?
+    const updatedGood = good + 1
+    setGood(updatedGood) 
+    console.log(good) //will log 0 on the first click
+  }
 
   return (
     <div className="container">
       <Header message = 'Please leave us some feedback'/>
+      <div className='motherOfButtons'>
+        <Button clickHandler = {handleClickPositive} text ="positive"/>
+        <Button clickHandler = {handleClick} text ="neutral"/>
+        <Button clickHandler = {handleClick} text ="negative"/>
+      </div>
     </div>
   )
 }
