09782b7463c9d178bc524196289085e4078a99bf
Margulis162 <markmargulis@proton.me>
2023-12-26 19:11:45 -0500

    header and soome css so my eyes do not hurt too much for unicafe exercises

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,4 +1,14 @@
 import { useState } from 'react'
+import './App.css'
+
+const Header = (props) =>{
+  return(
+    <div className='header'>
+      <h1>{props.message} </h1>
+    </div>
+  )
+}
+
 
 const App = () => {
   // save clicks of each button to its own state
@@ -7,8 +17,8 @@ const App = () => {
   const [bad, setBad] = useState(0)
 
   return (
-    <div>
-      code here
+    <div className="container">
+      <Header message = 'Please leave us some feedback'/>
     </div>
   )
 }
