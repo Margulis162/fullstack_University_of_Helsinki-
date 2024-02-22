f73fa78f2eea5c40f0ce99b0ade2ade4013172c6
Margulis162 <markmargulis@proton.me>
2024-02-22 06:04:31 -0500

    Some styles added and default ones delited.
    Flex css in index.css modified so items start more at the top of the page and have nice spacing. Also main font family changed to sanserif and letter spacing added for better readability. Most of the default unnecessary css delited from App.css. Also element local css added to List element.

--- part2/countries/src/components/list.jsx
+++ part2/countries/src/components/list.jsx
@@ -2,14 +2,22 @@ const List = ({items}) =>{
 
     //css
     const ulStyle = {
-        'list-style':'none'
+        'list-style':'none',
+        //flex
+        'display':'flex',
+        'flex-direction':'column',
+        'align-items':'flex-start'
+    }
+
+    const liStyle ={
+        'margin-block':'7px'
     }
 
     //js
     if(items === null){return null}else
     if(items.length > 10){return <p>Too many results, be more specific</p>}else  
     {
-        const lst = items.map((item) =><li><button>{item.name.common}</button></li>)
+        const lst = items.map((item) =><li style={liStyle}><button>{item.name.common}</button></li>)
         return(
             <ul style ={ulStyle}>
                 {lst}
