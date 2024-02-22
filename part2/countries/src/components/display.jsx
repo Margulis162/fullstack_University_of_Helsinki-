624b2862789b4e44b6f8b0a17163f7122446ba26
Margulis162 <markmargulis@proton.me>
2024-02-22 07:34:39 -0500

    Display element written.
    Display element written partially. Data destructured for rendering. The flag is still missing. Some layout and typography adjustment for better readability.

--- part2/countries/src/components/display.jsx
+++ part2/countries/src/components/display.jsx
@@ -1,8 +1,35 @@
 const Display =(props) => {
+    //css
+    const spanStyle ={
+        fontSize:'.8rem',
+        fontWeight:'bold',
+        marginInlineEnd:'10px',
+        color:'grey'
+    }
+    
+    const mainDiv = {
+        textAlign:"left"
+    }
+    //js
+    const  {name, capital, area, languages}  = props.item[0]
+    
+    const Lst = () =>{
+            const values = Object.values(languages)
+        return(
+            <ul>
+                {values.map((item) =><li key = {item}>{item}</li>)}
+            </ul>
+        )
+    }
     return(
-        <div>
-            <h1>info here</h1>
-            {console.log(props)}
+        <div style = {mainDiv}>
+            <h2><span style ={spanStyle}>Official name is:</span>{name.common}</h2>
+            <p><span style={spanStyle}>Capital:</span>{capital[0]}</p>
+            <p><span style={spanStyle}>Area:</span>{area}km</p>
+
+                <span style={spanStyle}>languages spoken:</span>
+                <Lst/>
+            
         </div>
     )
 }
