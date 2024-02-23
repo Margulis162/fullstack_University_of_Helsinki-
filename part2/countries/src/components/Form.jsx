250cad490151a1cce8118e06858097e6d4bb0901
Margulis162 <markmargulis@proton.me>
2024-02-23 03:29:45 -0500

    Exercises 2.18 and 2.19 are done
    Exercises 2.18 and 2.19 are done. 2.19 wants us to do buttons so the listed options are clickable. I have finished it yesterday by accident, because it was making sense. I also added some styles just for fun, and since I didn't mess with css for a while a went a bit wild. Styles a vary row, but shall live it as is for now, there are still a lot to do before I can jump to section 3.

--- part2/countries/src/components/Form.jsx
+++ part2/countries/src/components/Form.jsx
@@ -1,4 +1,24 @@
 const Form = ({text, handleChange, data}) =>{
+  // css
+  const styleH3 ={
+    display:'inline-block',
+    marginInlineEnd:'20px',
+    //typo
+    color:'rgb(200,200,200)'
+  }
+
+  const inputStyle={
+    backgroundImage:'linear-gradient(to right bottom,rgb(20, 29, 64),rgb(30, 29, 44),rgb(20, 41, 69)',
+    paddingInline:'15px',
+    blockSize:'30px',
+    border:'0px solid black',
+    borderRadius:'15px',
+    boxShadow:'inset 3px 3px 6px rgba(30, 20, 30, 1),inset -3px -3px 8px rgba(0, 255, 255, 0.3), -2px -2px 10px rgba(0, 20, 20,1),2px 2px 8px rgba(200, 255, 255, .5)',
+    //typography
+    fontSize:'18px',
+    color:'cyan',
+    letterSpacing:'.15rem'
+  }
   //the next part is to make sure the data are fetched before a search performed, sufficient for my purposes 
     if(data === null){
         return null
@@ -6,8 +26,8 @@ const Form = ({text, handleChange, data}) =>{
     return(
       <div>
       <form>
-        <h3>{text}</h3>
-        <input onChange={handleChange}/>
+        <h3 style={styleH3}>{text}</h3>
+        <input onChange={handleChange} style = {inputStyle}/>
       </form>
     </div>
     )
